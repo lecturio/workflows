09b5676bb37722462769ea0806db82a5e736552e
gogoluxecs <georgi.lambov@gmail.com>
2014-09-17 09:52:32 +0300

    Rebase when feature branch is on th go

--- commands/in-progress.sh
+++ commands/in-progress.sh
@@ -7,7 +7,6 @@ EXISTS_REMOTELY=`emit "git branch -r | grep origin/${WF_TASK}$"`
 
 if [ $EXISTS_LOCALLY -eq 0 ]; then
 	emit "git checkout $WF_TASK"
-	#emitgit_sync_branch $WF_TASK
 else
 	if [ $EXISTS_REMOTELY ]; then
 		emit "git checkout -b $WF_TASK origin/$WF_TASK" print_msg
@@ -17,3 +16,5 @@ else
 	fi
 	
 fi
+
+emitgit_sync_branch $WF_TASK
