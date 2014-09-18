4b1184a4f9a33751f90511b2ac6b8380458f0540
gogoluxecs <georgi.lambov@gmail.com>
2014-09-18 16:46:44 +0300

    deployable - ignore white spaces on feature rebase with master

--- commands/deployable.sh
+++ commands/deployable.sh
@@ -7,7 +7,7 @@ emit_failonerror "git pull --rebase origin master" print_msg
 
 emit "git checkout $WF_TASK" quiet
 emit_failonerror "git pull --rebase origin $WF_TASK"
-emit_failonerror "git rebase master" print_msg
+emit_failonerror "git rebase master --strategy -Xignore-all-space" print_msg
 
 emit "git checkout master" quiet
 emit_failonerror "git rebase $WF_TASK"
