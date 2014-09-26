7a55fc19b343b0e7436335d9e27d4dadc9a8d5a6
gogoluxecs <georgi.lambov@gmail.com>
2014-09-26 15:00:49 +0300

    removed rev-list check in resolved

--- commands/resolved.sh
+++ commands/resolved.sh
@@ -30,7 +30,8 @@ function sync_feature_changes() {
 	fi
 
 	emit "git checkout staging" quiet
-	emit "git rev-list --reverse ${CHERRY_PICK} | git cherry-pick -n --stdin"
+	#emit "git rev-list --reverse ${CHERRY_PICK} | git cherry-pick -n --stdin"
+	emit "git cherry-pick -n ${CHERRY_PICK}" 
 	print_msg "Check your changes before commit- possible data loss if merge is incorrect"
 }
 
