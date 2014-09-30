a353526d93f59452653dfd034c2e847f49df07d5
gogoluxecs <georgi.lambov@gmail.com>
2014-09-30 11:48:52 +0300

    try to avoid readlink command

--- workflow.sh
+++ workflow.sh
@@ -3,9 +3,12 @@
 # Entry point for workflow
 export WF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+if [ "`which readlink`" =! "" ]; then
 INSTALLED=`readlink /usr/local/bin/gitflow`
 if [ $INSTALLED != "" ]; then
 	export WF_DIR=`dirname $INSTALLED`
+fi
 fi
 
 export WF_TASK=$1
