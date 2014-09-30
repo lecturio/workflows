e4305eab61c6c71dda069727b2634f504d60b9b7
gogoluxecs <georgi.lambov@gmail.com>
2014-09-30 11:40:00 +0300

    Create usr/loca/bin if not exists

--- install/install.sh
+++ install/install.sh
@@ -3,5 +3,7 @@
 WF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 EXEC_DIR=`dirname $WF_DIR`
+
+sudo mkdir -p /usr/local/bin
 sudo rm -rf /usr/local/bin/gitflow
 sudo ln -s $EXEC_DIR/workflow.sh /usr/local/bin/gitflow
