6cd2c04bc88cd04518c9d4df96b45fd4848795b1
gogoluxecs <georgi.lambov@gmail.com>
2014-10-07 09:15:31 +0300

    Git prune before in-progress

--- commands/in-progress.sh
+++ commands/in-progress.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 emit "git fetch" quiet
+emit "git remote prune origin" quiet
 emit "git show-ref --verify refs/heads/$WF_TASK" quiet
 EXISTS_LOCALLY=$?
 EXISTS_REMOTELY=`emit "git branch -r --list origin/${WF_TASK}"`
