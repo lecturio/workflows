b9acfbceab9e72cdefa3bc2a2b089d8671b9a339
Svetoslav Denev <svetoslav.denev@lecturio.de>
2026-09-08 10:51:37 +0200

    Add git fetch command in deployable script
    This update introduces a 'git fetch' command in the deployable.sh script to ensure the latest changes are retrieved from the remote repository before proceeding with other operations. This enhancement improves the reliability of the deployment process.

--- commands/deployable.sh
+++ commands/deployable.sh
@@ -19,6 +19,7 @@ __merge_branch() {
 	emit_failonerror "git rebase -Xignore-all-space $2"
 }
 
+emit "git fetch" quiet
 emit "git remote prune origin" quiet
 
 require_origin_branch "$WF_PROD_BRANCH"
